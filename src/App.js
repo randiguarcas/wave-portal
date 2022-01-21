fdabb644f87904526bf75b752ac357542a1a76c8
Randi Guarcas <rguarcas18@gmail.com>
2022-01-20 23:03:16 -0600

    Added send waves

--- src/App.js
+++ src/App.js
@@ -9,52 +9,37 @@ function App() {
   const contractAddress = "0x36C33E5807B3D5f4E47C44838D96fF676EBC018d";
   const contractABI = abi.abi;
 
-  const checkIfWalletIsConnected = async () => {
-    const { ethereum } = window;
-
-    if (!ethereum) {
-      console.log("Make sure you have metamask");
-    } else {
-      console.log("We have the etherum object", ethereum);
-    }
-
-    const accounts = await ethereum.request({ method: "eth_accounts" });
-
-    if(accounts.length !== 0) {
-      const account = accounts[0];
-      console.log("Found an authorized account:", account);
-      setCurrentAccout(account);
-      console.log(currentAccount);
-    } else {
-      console.log("No authorized account found")
-    }
-  };
-
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
 
-      if(!ethereum) {
+      if (!ethereum) {
         alert("Get Metamask!");
         return;
       }
 
-      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
       console.log("connected", accounts[0]);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const wave = async () => {
     try {
       const { ethereum } = window;
-  
-      if(ethereum) {
+
+      if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-  
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
         let count = await wavePortalContract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
 
@@ -75,6 +60,27 @@ function App() {
   };
 
   useEffect(() => {
+    const checkIfWalletIsConnected = async () => {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask");
+      } else {
+        console.log("We have the etherum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccout(account);
+        console.log(currentAccount);
+      } else {
+        console.log("No authorized account found");
+      }
+    };
+
     checkIfWalletIsConnected();
   }, []);
 
@@ -83,7 +89,9 @@ function App() {
       <div className="App-header">
         <div>🤖</div>
 
-        <div className="bio">I am wave and this is my first smart contract.</div>
+        <div className="bio">
+          I am wave and this is my first smart contract.
+        </div>
 
         <button className="waveButton" onClick={wave}>
           Wave at me
@@ -91,9 +99,13 @@ function App() {
         <br></br>
         <button className="waveButton" onClick={connectWallet}>
           Connect Metamask &nbsp;
-          <img src="https://emoji.gg/assets/emoji/1385-metamask.png" width="30px" height="30px" alt="metamask" />
+          <img
+            src="https://emoji.gg/assets/emoji/1385-metamask.png"
+            width="30px"
+            height="30px"
+            alt="metamask"
+          />
         </button>
-
       </div>
     </div>
   );
